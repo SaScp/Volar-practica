8311f173147f12d045280ecc0f6cf67426f2d675
scorp <3048453@gmail.com>
2024-05-10 01:20:09 +0300

    set

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ function goToLink() {
 function goToLinkNotFound() {
     window.location.href ="/html/404.html";
 }
+function setSliderImg(obj) {
+    const images = document.querySelectorAll('.watch_img');
+    images.forEach((image) => {
+        console.log(image);
+        image.setAttribute('src', "/img/big_logo" + obj.id + ".svg");
+      });
+      
+}
